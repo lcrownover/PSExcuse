372542a0c8c6020659c05273e1c2178f5025fade
lcrownover <lcrownover127@gmail.com>
2017-04-04 13:50:54 -0700

    init

--- PSExcuse.psm1
+++ PSExcuse.psm1
@@ -1,1 +1,7 @@
-C:/Users/lucas.crownover/Google Drive/environment/WindowsPowerShell/Modules/PSExcuse/PSExcuse.psm1
+$Excuses = Get-Content "$($Env:PSModulePath.split(';')[0])\PSExcuse\excuses.txt"
+
+
+Function Get-Excuse {
+    $r = Get-Random -Maximum $($Excuses.Count + 1) -Minimum 1
+    Write-Output $Excuses[$r]
+}
